c29278d2cb336435ac0451980806524126dbbefc
suvajitcodewalnut <suvajit@codewalnut.com>
2025-11-06 16:58:14 +0530

    chore: format check

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,9 @@
 const Dashboard: React.FC = () => {
-  return (
-    <div>
-      <div className="font-bold">DASHBOARD</div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="font-bold">DASHBOARD</div>
+		</div>
+	);
 };
 
 export default Dashboard;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,21 +4,21 @@ import { useSidebar } from "../../store/useSidebar";
 import Dashboard from "../Dashboard/Dashboard";
 
 const Sidebar: React.FC = () => {
-  const size = useScreenSize();
-  const { isOpen } = useSidebar();
+	const size = useScreenSize();
+	const { isOpen } = useSidebar();
 
-  return (
-    <div
-      className={`${
-        size.width > 500 ? "w-95" : "w-80"
-      } z-1000 fixed top-0 right-0 h-full border-l-2 border-white bg-[#050a12] transform transition-transform duration-300 ${
-        isOpen ? "translate-x-0" : "translate-x-full"
-      }`}
-    >
-      <div className="text-white p-4 mt-20">
-        <Dashboard />
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`${
+				size.width > 500 ? "w-95" : "w-80"
+			} z-1000 fixed top-0 right-0 h-full border-l-2 border-white bg-[#050a12] transform transition-transform duration-300 ${
+				isOpen ? "translate-x-0" : "translate-x-full"
+			}`}
+		>
+			<div className="text-white p-4 mt-20">
+				<Dashboard />
+			</div>
+		</div>
+	);
 };
 export default Sidebar;
