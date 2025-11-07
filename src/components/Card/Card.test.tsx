05d3e34e875d0d425d15b9eb1222dfb87b858aee
suvajitcodewalnut <suvajit@codewalnut.com>
2025-11-08 00:47:22 +0530

    fix: missing handler fucn as props for card component

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -4,54 +4,55 @@ import Card from "./Card";
 import type { CardPropTypes } from "./Card.types";
 
 const renderComponent = (
-	title: string,
-	description: string,
-	priority: string,
-	assignedTo: string,
-	additional?: Partial<CardPropTypes>,
+  title: string,
+  description: string,
+  priority: string,
+  assignedTo: string,
+  additional?: Partial<CardPropTypes>
 ) =>
-	render(
-		<Card
-			title={title}
-			description={description}
-			priority={priority}
-			assignedTo={assignedTo}
-			markComplete={vi.fn()}
-			{...additional}
-		/>,
-	);
+  render(
+    <Card
+      title={title}
+      description={description}
+      priority={priority}
+      assignedTo={assignedTo}
+      markComplete={vi.fn()}
+      deleteTicket={vi.fn()}
+      {...additional}
+    />
+  );
 
 describe("Card", () => {
-	it("should render the Card component properly", () => {
-		renderComponent(
-			"Login fix",
-			"Users are unable to login",
-			"High",
-			"suvajit@codewalnut.com",
-		);
-	});
+  it("should render the Card component properly", () => {
+    renderComponent(
+      "Login fix",
+      "Users are unable to login",
+      "High",
+      "suvajit@codewalnut.com"
+    );
+  });
 
-	it('should render the "delete" button', () => {
-		renderComponent(
-			"Login fix",
-			"Users are unable to login",
-			"High",
-			"suvajit@codewalnut.com",
-		);
-		expect(
-			screen.getByRole("button", { name: "button-delete" }),
-		).toBeInTheDocument();
-	});
+  it('should render the "delete" button', () => {
+    renderComponent(
+      "Login fix",
+      "Users are unable to login",
+      "High",
+      "suvajit@codewalnut.com"
+    );
+    expect(
+      screen.getByRole("button", { name: "button-delete" })
+    ).toBeInTheDocument();
+  });
 
-	it('should render the "mark as complete" button', () => {
-		renderComponent(
-			"Login fix",
-			"Users are unable to login",
-			"High",
-			"suvajit@codewalnut.com",
-		);
-		expect(
-			screen.getByRole("button", { name: "button-mark-as-complete" }),
-		).toBeInTheDocument();
-	});
+  it('should render the "mark as complete" button', () => {
+    renderComponent(
+      "Login fix",
+      "Users are unable to login",
+      "High",
+      "suvajit@codewalnut.com"
+    );
+    expect(
+      screen.getByRole("button", { name: "button-mark-as-complete" })
+    ).toBeInTheDocument();
+  });
 });
