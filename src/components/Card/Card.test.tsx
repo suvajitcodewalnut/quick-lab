5af30d286a2b1e39badb5ef26cd7c379256ca0ca
suvajitcodewalnut <suvajit@codewalnut.com>
2025-11-08 16:57:34 +0530

    fix: failing test-case on pipeline

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -4,20 +4,23 @@ import Card from "./Card";
 import type { CardPropTypes } from "./Card.types";
 
 const renderComponent = (
+	id: string,
 	title: string,
 	description: string,
 	priority: string,
 	assignedTo: string,
+	isTicketResolved: boolean,
 	additional?: Partial<CardPropTypes>,
 ) =>
 	render(
 		<Card
+			id={id}
 			title={title}
 			description={description}
 			priority={priority}
 			assignedTo={assignedTo}
-			markComplete={vi.fn()}
-			deleteTicket={vi.fn()}
+			isTicketResolved={isTicketResolved}
+			onDeleteTicket={vi.fn()}
 			{...additional}
 		/>,
 	);
@@ -25,19 +28,23 @@ const renderComponent = (
 describe("Card", () => {
 	it("should render the Card component properly", () => {
 		renderComponent(
+			"1",
 			"Login fix",
 			"Users are unable to login",
 			"High",
 			"suvajit@codewalnut.com",
+			false,
 		);
 	});
 
 	it('should render the "delete" button', () => {
 		renderComponent(
+			"2",
 			"Login fix",
 			"Users are unable to login",
 			"High",
 			"suvajit@codewalnut.com",
+			false,
 		);
 		expect(
 			screen.getByRole("button", { name: "button-delete" }),
@@ -46,10 +53,12 @@ describe("Card", () => {
 
 	it('should render the "mark as complete" button', () => {
 		renderComponent(
+			"3",
 			"Login fix",
 			"Users are unable to login",
 			"High",
 			"suvajit@codewalnut.com",
+			true,
 		);
 		expect(
 			screen.getByRole("button", { name: "button-mark-as-complete" }),
